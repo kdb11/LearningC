38a669fe2d7a87d9bd523006c2b3a2faa0acdd94
Kevin Badwi <kevin.badwi@brightstraining.com>
2024-10-24 10:27:41 +0200

    added gitignore

--- obj/Debug/net8.0/gitExc.AssemblyInfo.cs
+++ obj/Debug/net8.0/gitExc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gitExc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a47e7bd6ce27bae337ac323860d1d9bfdde29f")]
 [assembly: System.Reflection.AssemblyProductAttribute("gitExc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gitExc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
